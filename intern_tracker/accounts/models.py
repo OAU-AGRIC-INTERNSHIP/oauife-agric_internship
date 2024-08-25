17d8c739533f34684eb174060b3762e7bbe37e7e
Praisesomiji <praise@gmail.com>
2024-08-25 05:00:05 +0100

    Add models for the accounts app

--- intern_tracker/accounts/models.py
+++ intern_tracker/accounts/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.models import User, Group
 from django.db import models
+from resources.models import Department
 
-# Create your models here.
+class Profile(models.Model):
+    intern = models.OneToOneField(User, on_delete=models.CASCADE)
+    matric_number = models.CharField(max_length=20)
+    department = models.ForeignKey(Department, on_delete=models.CASCADE)
+    whatsapp = models.CharField(max_length=15)
+
+class Team(Group):
+    lead = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name="lead_team")
+
