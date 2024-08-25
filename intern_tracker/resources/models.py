1e54902ce77ac817b0851d542ba902cc4a26fe26
Praisesomiji <praise@gmail.com>
2024-08-25 05:56:44 +0100

    Add models to resources app

--- intern_tracker/resources/models.py
+++ intern_tracker/resources/models.py
@@ -1,3 +1,70 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+class ResourceAbstract(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.name
+
+class Activity(ResourceAbstract):
+    pass
+
+class Course(ResourceAbstract):
+    pass
+
+class Crop(ResourceAbstract):
+    pass
+
+class Currency(ResourceAbstract):
+    pass
+
+class Department(ResourceAbstract):
+    pass
+
+class Grade(ResourceAbstract):
+    pass
+
+class Input(ResourceAbstract):
+    pass
+
+class Livestock(ResourceAbstract):
+    pass
+
+class Material(ResourceAbstract):
+    pass
+
+class Process(ResourceAbstract):
+    pass
+
+class RawMaterial(ResourceAbstract):
+    pass
+
+class Timeline(ResourceAbstract):
+    start_date = models.DateField()
+    end_date = models.DateField()
+
+class Unit(ResourceAbstract):
+    pass
+
+class Location(models.Model):
+    name = models.CharField(max_length=255)
+    address = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+class File(models.Model):
+    name = models.CharField(max_length=255)
+    file = models.FileField(upload_to='files/')
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
