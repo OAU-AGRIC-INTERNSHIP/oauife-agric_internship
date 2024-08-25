24181930df777f0a163bad2f94405c5c14ed9c5f
Praisesomiji <praise@gmail.com>
2024-08-25 05:40:25 +0100

    Register models in the production apps with admin site

--- intern_tracker/individual_production/admin.py
+++ intern_tracker/individual_production/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Production, Activity, Comment, Input, Harvesting, Marketing, Processing
 
-# Register your models here.
+admin.site.register(Production)
+admin.site.register(Activity)
+admin.site.register(Comment)
+admin.site.register(Input)
+admin.site.register(Harvesting)
+admin.site.register(Marketing)
+admin.site.register(Processing)
+
