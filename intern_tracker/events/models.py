11e15f28f3071d842a3b803baad99f8e55b78d1f
Praisesomiji <praise@gmail.com>
2024-08-25 05:08:02 +0100

    Add models to events app

--- intern_tracker/events/models.py
+++ intern_tracker/events/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from resources.models import Course, Location
 
-# Create your models here.
+class Class(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    day = models.CharField(max_length=20)
+    time = models.TimeField()
+    venue = models.ForeignKey(Location, on_delete=models.CASCADE)
+    link = models.URLField(blank=True, null=True)
+    information = models.TextField()
+    attendees = models.ManyToManyField('auth.Group')
+
+class Workshop(models.Model):
+    title = models.CharField(max_length=100)
+    date = models.DateField()
+    time = models.TimeField()
+    flyer = models.ImageField(upload_to='workshop_flyers/')
+
