29ea5168d941553df47615511e460baa326b8422
Praisesomiji <praise@gmail.com>
2024-08-25 05:37:35 +0100

    Create models for production apps

--- intern_tracker/individual_production/models.py
+++ intern_tracker/individual_production/models.py
@@ -1,3 +1,24 @@
-from django.db import models
+from intern_tracker.abstract_models import ProductionAbstract, ActivityAbstract, CommentAbstract, InputAbstract, HarvestingAbstract, MarketingAbstract, ProcessingAbstract
+from assignments.models import Proposal
 
-# Create your models here.
+class Production(ProductionAbstract):
+    proposal = models.ForeignKey(Proposal, on_delete=models.CASCADE)
+
+class Activity(ActivityAbstract):
+    production = models.ForeignKey(Production, on_delete=models.CASCADE)
+
+class Comment(CommentAbstract):
+    production = models.ForeignKey(Production, on_delete=models.CASCADE)
+
+class Input(InputAbstract):
+    production = models.ForeignKey(Production, on_delete=models.CASCADE)
+
+class Harvesting(HarvestingAbstract):
+    production = models.ForeignKey(Production, on_delete=models.CASCADE)
+
+class Marketing(MarketingAbstract):
+    production = models.ForeignKey(Production, on_delete=models.CASCADE)
+
+class Processing(ProcessingAbstract):
+    production = models.ForeignKey(Production, on_delete=models.CASCADE)
+
