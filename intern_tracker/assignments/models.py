95e17a7cf3ac4f924c8668c35f113596244f08f2
Praisesomiji <praise@gmail.com>
2024-08-25 05:03:32 +0100

    Create models for assignment app

--- intern_tracker/assignments/models.py
+++ intern_tracker/assignments/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import User
+from accounts.models import Team
+from resources.models import Timeline, Unit, Livestock, Crop, Location, File
 
-# Create your models here.
+class Teamwork(models.Model):
+    timeline = models.ForeignKey(Timeline, on_delete=models.CASCADE)
+    task = models.TextField()
+    team = models.ForeignKey(Team, on_delete=models.CASCADE)
+    unit = models.ForeignKey(Unit, on_delete=models.CASCADE)
+    livestock = models.ForeignKey(Livestock, on_delete=models.CASCADE, null=True, blank=True)
+    crop = models.ForeignKey(Crop, on_delete=models.CASCADE, null=True, blank=True)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+
+class Proposal(models.Model):
+    intern = models.ForeignKey(User, on_delete=models.CASCADE)
+    document = models.ForeignKey(File, on_delete=models.CASCADE)
+    timeline = models.ForeignKey(Timeline, on_delete=models.CASCADE)
+    task = models.TextField()
+    unit = models.ForeignKey(Unit, on_delete=models.CASCADE)
+    livestock = models.ForeignKey(Livestock, on_delete=models.CASCADE, null=True, blank=True)
+    crop = models.ForeignKey(Crop, on_delete=models.CASCADE, null=True, blank=True)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+    comments_by_supervisor = models.TextField(blank=True, null=True)
+    approved = models.BooleanField(default=False)
+
+class Special(models.Model):
+    timeline = models.ForeignKey(Timeline, on_delete=models.CASCADE)
+    task = models.TextField()
+    miscellaneous_group = models.ForeignKey(Team, on_delete=models.CASCADE)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+
