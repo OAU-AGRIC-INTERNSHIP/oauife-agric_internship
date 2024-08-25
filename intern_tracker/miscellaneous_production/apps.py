3c55755da9f8e2a0bf8492c1b4b65b86f43a00b7
Praisesomiji <praise@gmail.com>
2024-08-25 04:57:08 +0100

    Create the apps

---
+++ intern_tracker/miscellaneous_production/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MiscellaneousProductionConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'miscellaneous_production'
