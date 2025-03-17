4b2a75c5346d0ab12f085d2545efaf86d8877abc
Praisesomiji <praisesomiji@gmail.com>
2025-03-17 09:34:47 +0100

    Prep Django app for production

--- intern_tracker/intern_tracker/settings.py
+++ intern_tracker/intern_tracker/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-k@jc&y)9m!e#gz0waw!2_4v9fbg7_3r@-1)e5*%d1wc1apq6y7'
+from decouple import config
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
